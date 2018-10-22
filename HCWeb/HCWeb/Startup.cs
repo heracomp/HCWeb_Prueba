c96910493d902cf4df0c558a3003d4dae4cd5072
Abel <Abel@PC5>
2018-10-21 22:08:49 -0500

    HCWeb

---
+++ HCWeb/HCWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HCWeb.Startup))]
+namespace HCWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
